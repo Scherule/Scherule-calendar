23f60df7c0206345dcb2c51595bec165220ec3b8
Grzegorz Gurgul <gurgul.grzegorz@gmail.com>
2017-06-23 23:05:17 +0200

    Adding service discovery

--- src/main/kotlin/com.scherule.calendaring/CalendaringVerticle.kt
+++ src/main/kotlin/com.scherule.calendaring/CalendaringVerticle.kt
@@ -1,15 +1,31 @@
 package com.scherule.calendaring;
 
-import io.vertx.core.AbstractVerticle;
+import io.vertx.core.AbstractVerticle
+import io.vertx.servicediscovery.ServiceDiscovery.create
+import io.vertx.servicediscovery.ServiceDiscoveryOptions
+
 
 class CalendaringVerticle : AbstractVerticle() {
 
     override fun start() {
+        val discovery = create(vertx,
+                ServiceDiscoveryOptions()
+                        .setAnnounceAddress("scherule-announcing")
+                        .setName("calendaring"))
+
         vertx.createHttpServer()
                 .requestHandler({
-                    it.response().end("Hello Vert.x!")
+                    it.response().end("Hello Vert.x bla bla!")
                 })
-        .listen(8080);
+                .listen(config().getInteger("http.port", 8083), {
+                    if (it.succeeded()) {
+                        System.out.println("Server started");
+                    } else {
+                        System.out.println("Cannot start the server: " + it.cause());
+                    }
+                })
+
+        discovery.close()
     }
 
 }
